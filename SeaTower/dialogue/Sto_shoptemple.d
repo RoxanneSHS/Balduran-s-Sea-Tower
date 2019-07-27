a8f62b8eb9cdce3b7161acd5a0d2789d9748fba6
RoxanneSHS <cwillenbockel@aol.com>
2019-07-27 23:15:47 +0200

    v0.2

--- SeaTower/dialogue/Sto_shoptemple.d
+++ SeaTower/dialogue/Sto_shoptemple.d
@@ -31,5 +31,37 @@ IF~InParty("Rasaad")~THEN REPLY~Rassaad yn Bashir here is a sun soul monk. We'd 
 ++~Sorry, no but I've always loved to listen to the stories of the night white lady and the man in the moon and such.~ EXIT
 IF~!InParty("Rasaad")~THEN REPLY~No, but we're questers and seekers and may have use for what services you have to provide.~DO ~StartStore("olselune",LastTalkedToBy(Myself)) ~ EXIT
 
+CHAIN
+IF ~Global("Stoscales","Locals",2)~ THEN Stotronk Tronkscales11
+~Not too bad, given that one can hardly practice with that material beforehand.~
+=~May it serve you well.~DO~SetGlobal("Stoscales","Locals",3) GiveItemCreate("stobrarm",Player1,0,0,0)~EXIT
+
+CHAIN
+IF ~!Global("Stoscales","Locals",2)~ THEN Stotronk RoutineTronk
+~ 'Allo, friend, wot can I do for you this day?~
+END
+++~No need to trouble yourself, I'm just browsing about your smithy. I have heard much about it.~ + RoutineTronk2
+++~Could you show us what you have for sale?~DO~ StartStore("olblksmt",LastTalkedToBy(Myself))~EXIT
+IF~PartyHasItem("Stoscale")~THEN REPLY~Can you use these dragon scales for a purpose?~ + Tronkscales1
+++~Nothing, thanks. I was just leaving.~+ RoutineTronk2
+
+CHAIN
+IF ~~ THEN Stotronk RoutineTronk2
+~Thanks and fare thee well then.~EXIT
+
+CHAIN
+IF ~~ THEN Stotronk Tronkscales1
+~Oh, brown dragon scales, is it? I could attempt to make a scale armour from them. It will cost you a mere 5,000 gp.~
+END
+IF~PartyGoldLT(5000)~THEN REPLY~I don't have that much gold.~ + Tronkscales3
+IF~PartyGoldGT(4999)~THEN REPLY~Sure, go ahead.~DO~SetGlobal("Stoscales","Locals",2)TakePartyGold(5000) TakePartyItem("Stoscale") DestroyItem("Stoscale")~+ Tronkscales2
+++~I have no interest in it for the moment.~+ Tronkscales3
 
 
+CHAIN
+IF ~~ THEN Stotronk Tronkscales2
+~Fine, watch out, this is nothing one learns during the apprenticeship...~ DO~StartCutSceneMode()MoveViewObject("Stotronk",5) Wait(1) CreateVisualEffectObject("spcrtwpn","Stotronk") Wait(6) EndCutSceneMode() ActionOverride("Stotronk",Dialogue([PC]))~EXIT
+
+CHAIN
+IF ~~ THEN Stotronk Tronkscales3
+~I've no plans to shut down my business any time soon. Come back whenever you like.~EXIT
