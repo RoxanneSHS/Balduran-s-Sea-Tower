71d7ec612898bce020c337d14ed29e996bb845c7
RoxanneSHS <cwillenbockel@aol.com>
2019-07-28 17:24:50 +0200

    v0.2

--- SeaTower/dialogue/Sto_shoptemple.d
+++ SeaTower/dialogue/Sto_shoptemple.d
@@ -54,7 +54,7 @@ IF ~~ THEN Stotronk Tronkscales1
 ~Oh, brown dragon scales, is it? I could attempt to make a scale armour from them. It will cost you a mere 5,000 gp.~
 END
 IF~PartyGoldLT(5000)~THEN REPLY~I don't have that much gold.~ + Tronkscales3
-IF~PartyGoldGT(4999)~THEN REPLY~Sure, go ahead.~DO~SetGlobal("Stoscales","Locals",2)TakePartyGold(5000) TakePartyItem("Stoscale") DestroyItem("Stoscale")~+ Tronkscales2
+IF~PartyGoldGT(4999)~THEN REPLY~Sure, go ahead.~DO~SetGlobal("Stoscales","Locals",2)TakePartyGold(5000) TakePartyItem("Stoscale") DestroyItem("Stoscale") ActionOverride("Stotronk",Face(6))~+ Tronkscales2
 ++~I have no interest in it for the moment.~+ Tronkscales3
 
 
